6bcd2aafcf216775322addccb8108b86f197aeb5
James Couball <couballj@yahooinc.com>
2022-08-28 15:38:30 -0700

    Correctly us shell variables in Windows

--- test/capture2e_test.rb
+++ test/capture2e_test.rb
@@ -1,15 +1,37 @@
 require "test_helper"
 require 'open3'
+require 'fileutils'
 
 class Capture2eTest < Test::Unit::TestCase
-  def test_set_path
+  def test_echo_path
     output, status = Open3.capture2e(env, cmd, unsetenv_others: false)
     output.chomp!
     assert(status.exitstatus == 0)
     assert_equal(expected_path, output)
   end
 
+  def test_echo_path_from_script
+    create_test_script
+    output, status = Open3.capture2e(env, test_script_path, unsetenv_others: false)
+    output.chomp!
+    assert(status.exitstatus == 0)
+    assert_equal(expected_path, output)
+  end
+
+
   private
+
+  def test_script_path
+    File.expand_path('test_script')
+  end
+
+  def create_test_script
+    File.write(test_script_path, <<-SCRIPT)
+      #!/bin/bash
+      echo "$PATH"
+    SCRIPT
+    FileUtils.chmod('+x', test_script_path)
+  end
 
   def additional_path_element
     "testing"
