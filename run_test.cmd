622c967f73796148bf847351fcf65da6bc6ee3a3
James Couball <couballj@yahooinc.com>
2022-08-28 16:38:08 -0700

    Test git multi branch merge in windows

---
+++ run_test.cmd
@@ -0,0 +1,24 @@
+mkdir test_repo
+cd test_repo
+git config user.name "Test User"
+git config user.email "test@example.com"
+
+git init
+
+git checkout -b branch1
+echo "branch1" > file1.txt
+echo "branch1" > file2.txt
+git add file1.txt file2.txt
+git commit -m "Add file1 and file2 on branch1"
+
+git checkout master
+git checkout -b branch2
+echo "branch2" > file3.txt
+echo "branch2" > file4.txt
+git add file3.txt file4.txt
+git commit -m "Add file3 and file4 on branch2"
+
+git checkout master
+git merge branch1 branch2
+
+dir *.txt
